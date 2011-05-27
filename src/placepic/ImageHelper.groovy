99ef9fb6091f6fc9c5ad41daa2b1a3bae1b83897
Rob Fletcher <robert.w.fletcher@gmail.com>
2011-05-27 13:25:59 +0100

    refactored resize function into helper class

--- src/placepic/ImageHelper.groovy
+++ src/placepic/ImageHelper.groovy
@@ -2,10 +2,47 @@ package placepic
 
 import com.google.appengine.api.blobstore.*
 import com.google.appengine.api.datastore.*
+import com.google.appengine.api.images.*
 import static com.google.appengine.api.datastore.FetchOptions.Builder.*
 import static com.google.appengine.api.datastore.Query.FilterOperator.*
 
 class ImageHelper {
+	
+	static Image resizeTo(Entity entity, int width, int height) {
+		def image = entity.blob.image
+
+		def left = 0.0
+		def top = 0.0
+		def right = 1.0
+		def bottom = 1.0
+
+		def originalAspect = entity.height / entity.width
+		def desiredAspect = height / width
+
+		// resize to a square that will overflow the desired size
+		if (originalAspect < desiredAspect) {
+		    // original is more landscape than desired
+		    int resizeWidth = Math.ceil((entity.width / entity.height) * height)
+		    image.resize(resizeWidth, height)
+
+		    left = (1 - (width / image.width)) / 2
+		   right = 1 - left
+		} else {
+		    // original is more portrait than desired
+		    int resizeHeight = Math.ceil((entity.height / entity.width) * width)
+		    image.resize(width, resizeHeight)
+
+		    top = (1 - (height / image.height)) / 2
+		    bottom = 1 - top
+		}
+
+		image.transform {
+		    crop left, top, right, bottom
+			feeling lucky
+		}
+		
+		image
+	}
 
 	static Entity createFromBlob(datastore, BlobKey blob) {
 		def image = blob.image
--- war/WEB-INF/groovy/image.groovy
+++ war/WEB-INF/groovy/image.groovy
@@ -10,38 +10,7 @@ int height = Math.abs(params.height.toInteger())
 def entity = params.id ? ImageHelper.get(datastore, params.id) : ImageHelper.random(datastore)
 
 if (entity) {
-	def image = entity.blob.image
-
-	def left = 0.0
-	def top = 0.0
-	def right = 1.0
-	def bottom = 1.0
-
-	def originalAspect = entity.height / entity.width
-	def desiredAspect = height / width
-
-	// resize to a square that will overflow the desired size
-	if (originalAspect < desiredAspect) {
-	    // original is more landscape than desired
-	    int resizeWidth = Math.ceil((entity.width / entity.height) * height)
-	    image.resize(resizeWidth, height)
-
-	    left = (1 - (width / image.width)) / 2
-	   right = 1 - left
-	} else {
-	    // original is more portrait than desired
-	    int resizeHeight = Math.ceil((entity.height / entity.width) * width)
-	    image.resize(width, resizeHeight)
-
-	    top = (1 - (height / image.height)) / 2
-	    bottom = 1 - top
-	}
-
-	image.transform {
-	    crop left, top, right, bottom
-		feeling lucky
-	}
-
+	def image = ImageHelper.resizeTo(entity, width, height)
 	response.contentType = entity.contentType
 	sout << image.imageData
 } else {
