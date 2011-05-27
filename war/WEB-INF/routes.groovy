858d04afef341c82250f59488e6101d4b313bf71
Rob Fletcher <robert.w.fletcher@gmail.com>
2011-05-27 13:22:00 +0100

    updated routes

--- war/WEB-INF/routes.groovy
+++ war/WEB-INF/routes.groovy
@@ -1,9 +1,7 @@
-
-// routes for the blobstore service example
-get "/upload",  forward: "/upload.gtpl"
-get "/success", forward: "/success.gtpl"
 get "/image/@width/@height", forward: "/image.groovy?width=@width&height=@height",
 	validate: { width.isInteger() && width.toInteger() > 0 && height.isInteger() && height.toInteger() > 0 }
+
+get "/upload",  forward: "/upload.gtpl"
 get "/list", forward: "/list.gtpl"
 get "/delete/@id", forward: "/delete.groovy?id=@id"
 
